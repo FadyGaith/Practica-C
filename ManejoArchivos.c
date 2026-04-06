bdb26d92cd0d2c353d8693f7e768286b52a80d43
Fady <fadygaith19@gmail.com>
2026-04-05 22:55:26 -0300

    Codigos de practica para C

---
+++ ManejoArchivos.c
@@ -0,0 +1,45 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void LlenarTabla (int tabla[], int numero)
+{
+    for(int i = 0; i<numero; i++)
+    {
+        tabla[i] = (i+1)*5;
+    }
+}
+
+
+int main() {
+int numero;
+
+printf("Ingrese hasta que numero quiere ver la tabla del 5: ");
+fflush(stdout); // <--- AGREGAR ESTA LÍNEA
+scanf("%d", &numero);
+
+int *tabla = malloc(numero * sizeof(int));
+if (tabla == 0) {
+    printf("no se pudo asignar la memoria");
+    return 1; // <--- Esto corta el programa si no hay memoria
+}
+LlenarTabla(tabla, numero);
+
+printf("La tabla es: \n");
+for(int i = 0; i< numero; i++)
+{
+    printf("%d * 5 es: %d \n", i+1, tabla[i]);
+}
+
+FILE *MiArchivo = fopen("tabla.txt", "w");
+for(int i = 0; i< numero; i++)
+{
+    fprintf(MiArchivo,"%d * 5 es: %d \n", i+1, tabla[i]);
+}
+fclose(MiArchivo);
+
+free(tabla);
+
+    return 0;
+}
+
---
+++ ManejoStrings.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+int main (void)
+{
+    char palabra[20] = "HOLA ";
+    int largo = strlen(palabra);
+
+    printf("El largo de la palabra %s es: %d \n", palabra, largo);
+
+    char copia[10];
+
+    strcpy(copia, palabra);
+
+    printf("La palabra copiada es: %s con una longitud de: %d \n",copia, strlen(copia));
+
+    char nombre[] = "Fady";
+
+    strcat(palabra, nombre);
+
+    printf("Las palabras unidas son %s \n", palabra);
+
+    char contrasena[] = "123LoLa";
+    char IngresoContrasena[15];
+
+    printf("Ingrese una contrasena: \n");
+
+    scanf(" %[^\n]", IngresoContrasena);
+
+    if(!strcmp(IngresoContrasena,contrasena))
+        printf("Contrasena correcta \n");
+    else
+        printf("Contrasena incorrecta \n");
+    
+    return 0;
+}
